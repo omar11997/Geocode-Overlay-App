0929482efa089fce5014e7121fcd9d7c01515d0a
omar11997 <120408244+omar11997@users.noreply.github.com>
2023-03-25 01:46:02 +0200

    Add files via upload

--- streamlitmap.py
+++ streamlitmap.py
@@ -1,9 +1,9 @@
 import streamlit as st 
 import geopandas as gdp
 import leafmap.foliumap as leafmap
-from geopy.geocoders import Nominatim
 import folium
 import requests
+
 
 
 # ////////////title ////////////////
@@ -13,12 +13,12 @@ st.title("Geocoding & Overlay & Buffer App")
 
 # /////////////////// isert files ///////////////////
 uploaded_file = st.file_uploader("insert file", type = "geojson")
-uploaded_file2 = st.file_uploader("insert file 2 ", type= "geojson")
-uploaded_file3 = st.file_uploader("insert file 3 (to buffer) ", type= "geojson")
+uploaded_file2 = st.file_uploader("insert overlaped file  ", type= "geojson")
+uploaded_file3 = st.file_uploader("insert file to create buffer  and set the buffer lenght ", type= "geojson")
 # uploaded_file4 = st.file_uploader("insert file4", type= "geojson")
 
 
-address = st.text_input("Enter an address : ")
+address = st.text_input("search for address : ")
 
 
 
@@ -29,8 +29,19 @@ if uploaded_file and uploaded_file2 and uploaded_file3:
     filedata3 = gdp.read_file(uploaded_file3).to_crs("EPSG:3857")
     # filedata4 = gdp.read_file(uploaded_file4).to_crs("EPSG:3857")
 
+    # ////////////// map Creation /////////////
+    m = leafmap.Map()
     # ////////////// intersection ///////////////////////////
-    overlay = gdp.overlay(filedata1,filedata2, how = "intersection")
+    try:
+        overlay = gdp.overlay(filedata1,filedata2, how = "intersection")
+        m.add_gdf(overlay,style_function=lambda feature: {
+                    "fillColor": "red",
+                    "color": "black",
+                    "weight": 1,
+                    "fillOpacity": 0.5,
+                })
+    except:
+        pass
     # overlay.plot(color = 'red')
     # join = filedata1.sjoin(filedata4)
     # /////////////// buffer creation//////////////////
@@ -40,11 +51,12 @@ if uploaded_file and uploaded_file2 and uploaded_file3:
     m = leafmap.Map()
     # /////////////////////////////////////////////////
     length = st.text_input("Enter Distance of buffer:")
-    st.write("The Defult value of Buffer 5Km")
-    bu=st.button("make buffer")
+    st.write("The Defult value of Buffer 3Km")
+    bu =st.button("make buffer")
     if bu :
         if not length:
             length = 3000
+        length= int(length)
         buffer = filedata3["geometry"].buffer(length)
         map_buffer= gdp.GeoDataFrame(filedata3,geometry =buffer )
         m.add_gdf(map_buffer)
@@ -64,12 +76,13 @@ if uploaded_file and uploaded_file2 and uploaded_file3:
                   "weight": 1,
                   "fillOpacity": 0.5,
               })
-    m.add_gdf(overlay,style_function=lambda feature: {
-                  "fillColor": "red",
-                  "color": "black",
-                  "weight": 1,
-                  "fillOpacity": 0.5,
-              })
+    # if overlay:
+    #     m.add_gdf(overlay,style_function=lambda feature: {
+    #                 "fillColor": "red",
+    #                 "color": "black",
+    #                 "weight": 1,
+    #                 "fillOpacity": 0.5,
+    #             })
     # m.add_gdf(buffer_gdf)
     
     m.to_streamlit(height=500)
@@ -82,7 +95,7 @@ if address:
         map = leafmap.Map(location=[lat, lon], zoom_start=5)
         map.to_streamlit(height=500)
         folium.Marker([lat, lon],popup=address).add_to(map)
-        # folium.Marker([lat, lon]).add_to(map)
+        
     except:
         print("this address is not found ")
     
